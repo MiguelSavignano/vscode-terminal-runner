192fde0119a2020b0271fd913357ae49e390e5de
Miguel Savignano <migue.masx@gmail.com>
2017-09-08 21:34:55 +0200

    run command in new terminal

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,11 @@
 'use strict';
 import * as vscode from 'vscode';
+import terminalRunner from './terminal'
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "terminal-runner" is now active!');
   let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-    vscode.window.showInformationMessage('Hello World!');
+    terminalRunner.executeInTerminal("echo 'hola'")
   });
   context.subscriptions.push(disposable);
 }
